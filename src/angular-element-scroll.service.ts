eaea58351831d79eedba69a1cef18a64a441228e
Jack Fintan Tilly <jackftilly@gmail.com>
2017-03-16 14:24:21 -0400

    work on making the angular-element-scroll service be able to accept and store events

--- src/angular-element-scroll.service.ts
+++ src/angular-element-scroll.service.ts
@@ -1,14 +1,46 @@
+import { Injectable } from '@angular/core';
+
 import { ElementScrollEvent } from './angular-element-scroll-event';
 
+
 interface ElementScrollService {
-    queue: Array<ElementScrollEvent>
+    events: Array<ElementScrollEvent>
 }
+
+@Injectable()
 export class ElementScrollService {
 
-    perform(elementScrollEvent: ElementScrollEvent) {
-        this.queue.push(elementScrollEvent);
+    addEvent(elementScrollEvent: ElementScrollEvent) {
+        if (events.includes(elementScrollEvent)) {
+            console.log('You already added this event to the queue');
+            return events.indexOf(elementScrollEvent);
+        } else {
+            this.events.push(elementScrollEvent);
+            return events.length - 1;
+        }
+    }
 
-        // this is where the logic of the page scroll will live
+    perform(input: any) {
+        if (typeof(input) === 'ElementScrollEvent') {
+            this.performNew(input);
+        } else if (typeof(input) === 'number') {
+            this.performStored(input)
+        } else {
+            console.log('Error, input not known');
+        }
+    }
+
+    performStored(index: number) {
+        let elementScrollEvent = events[index];
+        this.runEvent(elementScrollEvent);
+    }
+
+    performNew(elementScrollEvent: ElementScrollEvent) {
+        this.runEvent(elementScrollEvent);
+    }
+
+    private runEvent(event: ElementScrollEvent) {
+        // this is where the logic will live
     }
 
 }
