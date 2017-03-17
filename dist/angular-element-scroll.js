9e33e269bd1bbd9ac22712fc97864a8c29540cad
Jack <jackftilly@gmail.com>
2017-03-17 18:09:26 -0400

    still trying to make module work as a npm dependency: :

---
+++ dist/angular-element-scroll.js
@@ -0,0 +1,5 @@
+var ElementScrollEvent = require('../src/angular-element-scroll-event');
+var ElementScrollService = require('../src/angular-element-scroll.service');
+
+module.exports.ElementScrollEvent = ElementScrollEvent;
+module.exports.ElementScrollService = ElementScrollService;
